6acf8cd44e7925dc4246542d5ccb981cf7060d5a
Tilda Kampman <tilda.kampman@gmail.com>
2022-11-16 13:25:02 +0100

    update continents

--- Game.java
+++ Game.java
@@ -5,30 +5,33 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 
 public class Game {
-    public static void main(String[] args) {
-	System.out.println("Countries Game");
-	Console console = System.console();
-	BufferedReader br = null;
-	try {
-	    br = new BufferedReader(new 
-FileReader("continents.csv"));
-	    String line;
-	    while((line=br.readLine()) != null) {
-		String[] cols = line.split(",");
-		String country = cols[0];
-		String continent= cols[1];
-		String answer= console.readLine("which continent does 
-" + country + " belong to?");
-	if(answer.equals(continent)) {
-	    System.out.println("Correct!");
-	}else {
-	    System.out.println("No, the answer is " + continent);
+	public static void main(String[] args) {
+		System.out.println("Countries Game");
+		Console console = System.console();
+		BufferedReader br = null;
+		int total = 0;
+		int correct = 0;
+		try {
+			br = new BufferedReader(new FileReader("continents.csv"));
+			String line;
+			while ((line = br.readLine()) != null) {
+				total++;
+				String[] cols = line.split(",");
+				String country = cols[0];
+				String continent = cols[1];
+				String answer = console.readLine("which continent does " + country + " belong to? ");
+				if (answer.equalsIgnoreCase(continent)) {
+					correct++;
+					System.out.println("Correct!");
+				} else {
+					System.out.println("No, the answer is " + continent);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("You scored " + correct + " out of " + total);
 	}
-    }
-	}catch(FileNotFoundException e) {
-	    e.printStackTrace();
-	}catch(IOException e) {
-	    e.printStackTrace();
-	}
-    }
-
+}
